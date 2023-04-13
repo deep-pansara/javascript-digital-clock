6b02dbe446a54e513979299256f32b4c0cb85c17
Deep Pansara <87435226+deep-pansara@users.noreply.github.com>
2023-04-13 11:36:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const ampm = document.querySelector(".ampm") 
+
+
+let count=0
+function digitalClock() {
+
+    let clock = new Date();
+    let h = clock.getHours();
+    let m = clock.getMinutes();
+    let s = clock.getSeconds();
+    let pm = "AM"
+
+    if (h >= 12){
+        pm = "PM"
+    }
+
+    if (s >= 10){
+        seconds.innerHTML = s;
+    }else {
+        seconds.innerHTML =`0${s}`;
+    };
+
+
+    if (m >= 10){
+        minutes.innerHTML = m;
+    }else {
+        minutes.innerHTML =`0${m}`;
+    };
+
+    if(h >= 12){
+        h = h-12;
+        h = `0${h}`
+    }
+    
+    if (h <= 10){
+        hours.innerHTML = h;
+    }else {
+        hours.innerHTML =`0${h}`;
+    };
+
+
+
+
+
+    
+    console.log("Hello There " + count++ , h, m , s);
+
+
+    hours.innerHTML = h;
+    ampm.innerHTML = pm;
+
+};
+
+setInterval(()=>{
+    digitalClock();
+},1000)
